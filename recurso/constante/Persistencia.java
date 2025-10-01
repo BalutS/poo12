8b9e78c149219befbc4338fd02e4ba9d6cb3e48e
BalutS <sotocanomartin830@gmail.com>
2025-10-01 11:14:32 -0500

    Update Persistencia.java

--- recurso/constante/Persistencia.java
+++ recurso/constante/Persistencia.java
@@ -4,16 +4,16 @@ import java.io.File;
 
 public class Persistencia {
     public static final String RUTA_PROYECTO = System.getProperty("user.dir");
-    public static final String NOMBRE_CARPETA = "miBaseDeDatos";
-    public static final String SEPARADOR_CARPETAS = File.separator;
-
-    public static final String RUTA_BASE_DATOS = RUTA_PROYECTO + SEPARADOR_CARPETAS + NOMBRE_CARPETA + SEPARADOR_CARPETAS;
-
     public static final String NOMBRE_BASE_DATOS_GENERO = "Genero.txt";
     public static final String NOMBRE_BASE_DATOS_PELICULA = "Pelicula.txt";
-
-    public static final String RUTA_GENERO_TXT = RUTA_BASE_DATOS + NOMBRE_BASE_DATOS_GENERO;
-    public static final String RUTA_PELICULA_TXT = RUTA_BASE_DATOS + NOMBRE_BASE_DATOS_PELICULA;
+    public static final String NOMBRE_CARPETA = "Mi base de datos";
 
     public static final String SEPARADOR_COLUMNAS = ";";
+    public static final String SEPARADOR_CARPETAS = java.io.File.separator;
+
+    public static final String NOMBRE_GENERO = RUTA_PROYECTO + SEPARADOR_CARPETAS + NOMBRE_CARPETA +
+            SEPARADOR_CARPETAS + NOMBRE_BASE_DATOS_GENERO;
+
+    public static final String NOMBRE_PELICULA = RUTA_PROYECTO + SEPARADOR_CARPETAS + NOMBRE_CARPETA +
+            SEPARADOR_CARPETAS + NOMBRE_BASE_DATOS_PELICULA;
 }
